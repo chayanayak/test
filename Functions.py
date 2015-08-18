328257b0564d0c307a4936397ef00796fe27edcc
Chaya Nayak <chaya@premise.com>
2015-08-17 21:19:42 -0700

    initial commit

---
+++ Functions.py
@@ -0,0 +1,10 @@
+def my_func(x, y):
+    z=x+y
+    return z
+
+print(my_func(2, 4))
+
+if __name__ == '__main__':
+    output = my_func(9, 10)
+    print(output)
+
---
+++ learnmain.py
@@ -0,0 +1,7 @@
+import Functions
+
+print(Functions.my_func(10,50000))
+
+if __name__ == '__main__':
+    print(Functions.my_func(2,3))
+
